7a215d90bb16a08043fdc1edbf9736ea95fc661d
Stefan Sevelda <stefan.sevelda@technikum-wien.at>
2014-03-26 13:32:49 +1100

    fixed issue #31

--- Rede/manhattanplot/man_graph.js
+++ Rede/manhattanplot/man_graph.js
@@ -80,6 +80,8 @@ function reset_manhattan() {
     create_label_button();
     // load statistical values
     load_stat_value(file_json);
+    // SNP list
+    histogram_degree_SNPs(file_json, 0);
 
     data_from_HDS == "no";
 
