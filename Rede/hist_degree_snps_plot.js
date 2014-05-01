f6defea1ba9ee4fac5ed811f71cd213f2cd737d1
stefan-sev <stefan.sevelda@hotmail.com>
2014-05-01 17:12:12 +1000

    refactored the list on the left side of the UI

--- Rede/hist_degree_snps_plot.js
+++ Rede/hist_degree_snps_plot.js
@@ -11,250 +11,246 @@
  * @param {number} probe_group
  */
     
-function histogram_degree_SNPs(file_name, probe_group, if_zoom, if_stat_brush) {
+function histogram_degree_SNPs(probe_group, if_zoom, if_stat_brush) {
     
     var data_degree_snp = new Array();
     var allNodes_hes = new Array();
     var links_hes = new Array();
 
-    d3.json(file_name, function(json) {
-        // filter for the zoom function of the arc plot
-        if (if_zoom) {
-            zoom_links.forEach( function(d) {
-                links_hes.push(d);
-            });
-            zoom_allNodes.forEach( function(d) {
-                allNodes_hes.push(d);
-                data_degree_snp.push(d);
-            });
-            
+    // filter for the zoom function of the arc plot
+    if (if_zoom) {
+        zoom_links.forEach( function(d) {
+            links_hes.push(d);
+        });
+        zoom_allNodes.forEach( function(d) {
+            allNodes_hes.push(d);
+            data_degree_snp.push(d);
+        });
 
-        } else if(if_stat_brush) {
-            stat_links.forEach( function(d) {
+
+    } else if(if_stat_brush) {
+        stat_links.forEach( function(d) {
+            links_hes.push(d);
+        });
+        stat_allNodes.forEach( function(d) {
+            allNodes_hes.push(d);
+            data_degree_snp.push(d);
+        });
+        
+    } else {
+        links.forEach( function(d) {
+            if (probe_group === 0) {
                 links_hes.push(d);
-            });
-            stat_allNodes.forEach( function(d) {
-                allNodes_hes.push(d);
-                data_degree_snp.push(d);
-            });
-            // clear the variables for the next selection
-            stat_allNodes = [];
-            stat_links = [];
-        } else {
-            json.links.forEach( function(d) {
-                if (probe_group === 0) {
+            } else {
+                if (d.probe_group === probe_group) {
                     links_hes.push(d);
-                } else {
-                    if (d.probe_group === probe_group) {
-                        links_hes.push(d);
-                    }
                 }
-            });
+            }
+        });
 
-            json.nodes.forEach(function(d) {
-                if (probe_group === 0) {
-                    allNodes_hes.push(d);
+        allNodes.forEach(function(d) {
+            if (probe_group === 0) {
+                allNodes_hes.push(d);
+                data_degree_snp.push(d);
+            } else {
+                allNodes_hes.push(d);
+                if (d.probe_group === probe_group) {
                     data_degree_snp.push(d);
-                } else {
-                    allNodes_hes.push(d);
-                    if (d.probe_group === probe_group) {
-                        data_degree_snp.push(d);
-                    }
                 }
-            });
-        }
-        //it will create the histogram degree X SNPs in circle_plot
-        var margin = {
-            top: 50,
-            right: 20,
-            bottom: 50,
-            left: 400
-        },
+            }
+        });
+    }
+    //it will create the histogram degree X SNPs in circle_plot
+    var margin = {
+        top: 50,
+        right: 20,
+        bottom: 50,
+        left: 400
+    },
 
         width = 700 - margin.left - margin.right; 
 
-        if (probe_group == 0) {
-            if (data_degree_snp.length > 10) {
-                var height = 34.1796875 * allNodes_hes.length - margin.top - margin.bottom; //200
-            } else {
-                var height = 34.1796875 * 10 - margin.top - margin.bottom;
-            }
+    if (probe_group == 0) {
+        if (data_degree_snp.length > 10) {
+            var height = 34.1796875 * allNodes_hes.length - margin.top - margin.bottom; //200
         } else {
-            if (data_degree_snp.length > 10) {
-                var height = 34.1796875 * data_degree_snp.length - margin.top - margin.bottom; //200
-            } else {
-                var height = 34.1796875 * 10 - margin.top - margin.bottom;
-            }
+            var height = 34.1796875 * 10 - margin.top - margin.bottom;
         }
+    } else {
+        if (data_degree_snp.length > 10) {
+            var height = 34.1796875 * data_degree_snp.length - margin.top - margin.bottom; //200
+        } else {
+            var height = 34.1796875 * 10 - margin.top - margin.bottom;
+        }
+    }
 
-        var y = d3.scale.ordinal()
-            .rangeRoundBands([0, height], .5);
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([0, height], .5);
 
-        var x = d3.scale.linear()
-            .range([0, width]);
+    var x = d3.scale.linear()
+        .range([0, width]);
 
-        var xAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
+    var xAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
-        var yAxis = d3.svg.axis()
-            .scale(x)
-            .orient("top");
+    var yAxis = d3.svg.axis()
+        .scale(x)
+        .orient("top");
 
-        var svg = d3.select("#hds_matrix").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("#hds_matrix").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        y.domain(data_degree_snp.map(function(d, i) {
-            return "id:" + d.id + " chr" + d.chrom + ':' + d.bp_position + " " + d.rs +
-                " probe_group:" + d.probe_group;
-        }));
+    y.domain(data_degree_snp.map(function(d, i) {
+        return "id:" + d.id + " chr" + d.chrom + ':' + d.bp_position + " " + d.rs +
+        " probe_group:" + d.probe_group;
+    }));
 
-        x.domain([0, d3.max(data_degree_snp, function(d) {
-            return d.degree;
-        })]);
+    x.domain([0, d3.max(data_degree_snp, function(d) {
+        return d.degree;
+    })]);
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .call(xAxis);
+    svg.append("g")
+        .attr("class", "x axis")
+        .call(xAxis);
 
-        // now rotate text on x axis
-        // first move the text left so no longer centered on the tick
-        // then rotate up to get 90 degrees.
-        svg.selectAll(".x text")
-            .style("font-size", "14px")
-            .attr("transform", function(d) {
-                return "translate(" + this.getBBox().height * -0.5 + "," + this.getBBox().height * 0 + ")rotate(0)";
-            })
-            .on("click", function(d, i) {
-                var person = prompt("\n1) ClinVar\n2) dbSNP\n3) Ensembl\n4) PheGenI\n5) OMIM\n6) openSNP\n"
-                    + "7) SNPedia\n8) UCSC");
+    // now rotate text on x axis
+    // first move the text left so no longer centered on the tick
+    // then rotate up to get 90 degrees.
+    svg.selectAll(".x text")
+        .style("font-size", "14px")
+        .attr("transform", function(d) {
+            return "translate(" + this.getBBox().height * -0.5 + "," + this.getBBox().height * 0 + ")rotate(0)";
+        })
+    .on("click", function(d, i) {
+        var person = prompt("\n1) ClinVar\n2) dbSNP\n3) Ensembl\n4) PheGenI\n5) OMIM\n6) openSNP\n"
+            + "7) SNPedia\n8) UCSC");
 
-                if (person != null) {
-                    if ("8" == person) {
-                        html = 'http://genome.ucsc.edu/cgi-bin/hgTracks?org=human&db=hg19&position=' + 'chr' +
-                            data_degree_snp[i].chrom + ':' + (data_degree_snp[i].bp_position - 1000) + '-' + (data_degree_snp[i].bp_position + 1000);
+        if (person != null) {
+            if ("8" == person) {
+                html = 'http://genome.ucsc.edu/cgi-bin/hgTracks?org=human&db=hg19&position=' + 'chr' +
+        data_degree_snp[i].chrom + ':' + (data_degree_snp[i].bp_position - 1000) + '-' + (data_degree_snp[i].bp_position + 1000);
 
-                    } else if ("6" == person) {
-                        html = 'http://opensnp.org/snps/' + data_degree_snp[i].rs;
+            } else if ("6" == person) {
+                html = 'http://opensnp.org/snps/' + data_degree_snp[i].rs;
 
-                    } else if ("2" == person) {
-                        html = 'http://www.ncbi.nlm.nih.gov/SNP/snp_ref.cgi?rs=' + data_degree_snp[i].rs.substring(2);
+            } else if ("2" == person) {
+                html = 'http://www.ncbi.nlm.nih.gov/SNP/snp_ref.cgi?rs=' + data_degree_snp[i].rs.substring(2);
 
-                    } else if ("4" == person) {
-                        html = 'http://www.ncbi.nlm.nih.gov/gap/phegeni?tab=2&rs=' + data_degree_snp[i].rs.substring(2);
+            } else if ("4" == person) {
+                html = 'http://www.ncbi.nlm.nih.gov/gap/phegeni?tab=2&rs=' + data_degree_snp[i].rs.substring(2);
 
-                    } else if ("3" == person) {
-                        html = 'http://www.ensembl.org/Homo_sapiens/Variation/Summary?r=' + data_degree_snp[i].chrom + ':' +
-                            (data_degree_snp[i].bp_position - 1000) + '-' + (data_degree_snp[i].bp_position + 1000) + ';source=dbSNP;v=rs' +
-                            data_degree_snp[i].rs.substring(2) + ';vdb=variation';
+            } else if ("3" == person) {
+                html = 'http://www.ensembl.org/Homo_sapiens/Variation/Summary?r=' + data_degree_snp[i].chrom + ':' +
+        (data_degree_snp[i].bp_position - 1000) + '-' + (data_degree_snp[i].bp_position + 1000) + ';source=dbSNP;v=rs' +
+        data_degree_snp[i].rs.substring(2) + ';vdb=variation';
 
-                    } else if ("7" == person) {
-                        html = 'http://www.snpedia.com/index.php/Rs' + data_degree_snp[i].rs.substring(2);
+            } else if ("7" == person) {
+                html = 'http://www.snpedia.com/index.php/Rs' + data_degree_snp[i].rs.substring(2);
 
-                    } else if ("5" == person) {
-                        html = 'http://omim.org/search?index=entry&search=rs' + data_degree_snp[i].rs.substring(2);
+            } else if ("5" == person) {
+                html = 'http://omim.org/search?index=entry&search=rs' + data_degree_snp[i].rs.substring(2);
 
-                    } else if ("1" == person) {
-                        html = 'http://www.ncbi.nlm.nih.gov/clinvar?term=rs' + data_degree_snp[i].rs.substring(2);
-                    }
-                    window.open(html)
-                }
-            });
+            } else if ("1" == person) {
+                html = 'http://www.ncbi.nlm.nih.gov/clinvar?term=rs' + data_degree_snp[i].rs.substring(2);
+            }
+            window.open(html)
+        }
+    });
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(0," + 0 + ")")
-            .call(yAxis)
-            .append("text")
-            .attr("transform", "rotate(0)")
-            .attr("x", width / 2)
-            .attr("y", -25).style("font-size", "15px")
-            .style("text-anchor", "end")
-            .text("Degree");
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(0," + 0 + ")")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(0)")
+        .attr("x", width / 2)
+        .attr("y", -25).style("font-size", "15px")
+        .style("text-anchor", "end")
+        .text("Degree");
 
-        var bar_hDS = svg.selectAll(".bar")
-            .data(data_degree_snp)
-            .enter().append("rect")
-            .attr("class", "ds bar")
-            .attr("y", function(d) {
-                return y("chr" + d.chrom + ':' + d.bp_position);
-            })
-            .attr("height", y.rangeBand())
-            .attr("x", 0)
-            .attr("width", function(d) {
-                return x(d.degree);
-            })
-            .on("mousedown", function(g, i) {
-                bar_hDS.style("fill", "steelblue");
-                l = [];
-                list_idx_in_links = [];
-                list_idx_in_links2 = [];
-                
-				// filters the selected SNPs and write it in the arrays for the
-				// SNPs paragraphs.
-                links_hes.forEach(function(e) {
-                    if (g.id === e.source || g.id === e.target) {
-                        l.push(("chr" + allNodes[e.source].chrom + ':' +
-                                allNodes[e.source].bp_position));
-                        l.push(("chr" + allNodes[e.target].chrom + ':' +
-                                allNodes[e.target].bp_position));
-                        list_idx_in_links.push(e.source + "-" + e.target);
-                        list_idx_in_links2.push(e);
-                    }
-                });	
+    var bar_hDS = svg.selectAll(".bar")
+        .data(data_degree_snp)
+        .enter().append("rect")
+        .attr("class", "ds bar")
+        .attr("y", function(d) {
+            return y("chr" + d.chrom + ':' + d.bp_position);
+        })
+    .attr("height", y.rangeBand())
+        .attr("x", 0)
+        .attr("width", function(d) {
+            return x(d.degree);
+        })
+    .on("mousedown", function(g, i) {
+        bar_hDS.style("fill", "steelblue");
+        l = [];
+        list_idx_in_links = [];
+        list_idx_in_links2 = [];
 
-                bar_hDS.filter(function(d) {
+        // filters the selected SNPs and write it in the arrays for the
+        // SNPs paragraphs.
+        links_hes.forEach(function(e) {
+            if (g.id === e.source || g.id === e.target) {
+                l.push(("chr" + allNodes[e.source].chrom + ':' +
+                        allNodes[e.source].bp_position));
+                l.push(("chr" + allNodes[e.target].chrom + ':' +
+                        allNodes[e.target].bp_position));
+                list_idx_in_links.push(e.source + "-" + e.target);
+                list_idx_in_links2.push(e);
+            }
+        });	
+
+        bar_hDS.filter(function(d) {
+            if (include_in_arr(l, ("chr" + d.chrom + ':' + d.bp_position))) {
+                return d;
+            }
+        })
+        .transition()
+            .style("fill", "red");
+
+        bar_hDS.filter(function(d) {
+            if (("chr" + d.chrom + ':' + d.bp_position) === ("chr" + data_degree_snp[i].chrom + ':' + data_degree_snp[i].bp_position)) {
+                return d;
+            }
+        })
+        .transition()
+            .style("fill", "#32ee00");
+
+        if(plot_chosen == "p_cir" || plot_chosen == "p_arc") {
+            d3.select("#chart").selectAll("g circle").transition().style("opacity", 0);
+
+            d3.select("#chart").selectAll("g circle") //select the circles           
+                .filter(function(d) {
                     if (include_in_arr(l, ("chr" + d.chrom + ':' + d.bp_position))) {
                         return d;
                     }
                 })
-                    .transition()
-                    .style("fill", "red");
+            .transition()
+                .style("opacity", 1);
 
-                bar_hDS.filter(function(d) {
-                    if (("chr" + d.chrom + ':' + d.bp_position) === ("chr" + data_degree_snp[i].chrom + ':' + data_degree_snp[i].bp_position)) {
+            d3.select("#chart").selectAll(".link").transition().style("opacity", 0);
+
+            //select the association regarding to the circle selected
+            d3.select("#chart").selectAll(".link") 
+                .filter(function(d, i) {
+                    if (include_in_arr(list_idx_in_links, d.source + "-" + d.target)) {
                         return d;
                     }
                 })
-                    .transition()
-                    .style("fill", "#32ee00");
-
-                if(plot_chosen == "p_cir" || plot_chosen == "p_arc") {
-                    d3.select("#chart").selectAll("g circle").transition().style("opacity", 0);
-
-                    d3.select("#chart").selectAll("g circle") //select the circles           
-                        .filter(function(d) {
-                            if (include_in_arr(l, ("chr" + d.chrom + ':' + d.bp_position))) {
-                                return d;
-                            }
-                        })
-                        .transition()
-                        .style("opacity", 1);
-
-                    d3.select("#chart").selectAll(".link").transition().style("opacity", 0);
-
-                    //select the association regarding to the circle selected
-                    d3.select("#chart").selectAll(".link") 
-                        .filter(function(d, i) {
-                            if (include_in_arr(list_idx_in_links, d.source + "-" + d.target)) {
-                                return d;
-                            }
-                        })
-                        .transition()
-                        .style("opacity", 0.3);
-                }
-            });
-
-        svg.selectAll(".bar") //show degree as tooltip - title
-            .data(data_degree_snp)
-            .append("title")
-            .text(function(d) {
-                return "chr" + d.chrom + ':' + d.bp_position + " ; " + d.degree;
-            });
+            .transition()
+                .style("opacity", 0.3);
+        }
     });
-}
+
+    svg.selectAll(".bar") //show degree as tooltip - title
+        .data(data_degree_snp)
+        .append("title")
+        .text(function(d) {
+            return "chr" + d.chrom + ':' + d.bp_position + " ; " + d.degree;
+        });
+};
 
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ histogram degree X SNPs (matrix) ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
--- Rede/hist_edges_subgraphID_plot.js
+++ Rede/hist_edges_subgraphID_plot.js
@@ -9,7 +9,12 @@
  *     When a bar is clicked a subgraph is selected.
  * @param {string} file_name
  */
-function histogram_edges_subgraphId(file_name, if_zoom) {
+function histogram_edges_subgraphId(if_zoom) {
+
+    var data_probe_group1 = [];
+    var data_probe_group2 = [];
+    var data_obj = []; //array with obj. with the couple egds and probe_group
+
     var data_probe_group1 = [];
     //this array will receive the probe_group of the json file, example -> [1,3,2,4,1,1,3,4,4,4,2] .
     //Next it will be sorted,  exemplo -> [1,1,1,2,2,3,3,4,4,4]
@@ -17,13 +22,12 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
     //this array will receive small array from data_probe_group1.
     //the length the each sub-array represent the number the edges in a subgraph, 
     //example -> [[1,1,1],[2,2],[3,3],[4,4,4]], in this array the 1ª element is the subgraph 1 and has 3 edges.    
-    d3.json(file_name, function(json) {
         if (if_zoom) {
             zoom_links.forEach( function(d) {
                 data_probe_group1.push(d.probe_group);
             });
         } else {
-            json.links.forEach(function(d) {
+            links.forEach(function(d) {
                 data_probe_group1.push(d.probe_group);
             }); //-> [1,3,2,4,1,1,3,4,4,4,2]
         }
@@ -31,33 +35,27 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
             return a > b ? 1 : 0;
         });
 
-        create_data_probe_group2()
+        //this function will create the array data_probe_group2 from data_probe_group1,
+        //exemplo: [1,1,1,2,2,3,3,4,4,4] -> [[1,1,1],[2,2],[3,3],[4,4,4]].
+        var max = d3.max(data_probe_group1, function(d) {
+            return d;
+        });
+        var min = d3.min(data_probe_group1, function(d) {
+            return d;
+        });
+        var li, ary;
 
-        function create_data_probe_group2() {
-            //this function will create the array data_probe_group2 from data_probe_group1,
-            //exemplo: [1,1,1,2,2,3,3,4,4,4] -> [[1,1,1],[2,2],[3,3],[4,4,4]].
-            var max = d3.max(data_probe_group1, function(d) {
-                return d;
-            });
-            var min = d3.min(data_probe_group1, function(d) {
-                return d;
-            });
-            var li, ary;
-
-            while (min < max) {
-                li = data_probe_group1.lastIndexOf(min);
-                ary = data_probe_group1.splice(0, li + 1);
-                data_probe_group2.push(ary);
-                min = d3.min(data_probe_group1, function(d) {
+        while (min <= max) {
+            li = data_probe_group1.lastIndexOf(min);
+            ary = data_probe_group1.splice(0, li + 1);
+            data_probe_group2.push(ary);
+            min = d3.min(data_probe_group1, function(d) {
                     return d;
-                })
-            }
-            data_probe_group2.push(data_probe_group1);
+            })            
         }
 
-        var data_obj = []; //array with obj. with the couple egds and probe_group
 
-        function creat_obj(probe_group, edgs) {
+        function creat_obj_probe(probe_group, edgs) {
             //function to create the obj. with the couple egds and probe_group
             var obj = {};
             obj.n_probe_group = probe_group;
@@ -67,7 +65,7 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
 
         for (var i = 0; i < data_probe_group2.length; i++) {
             // from data_probe_group2 we will create a array with obj. with the couple egds and probe_group
-            data_obj.push(creat_obj(data_probe_group2[i][0], data_probe_group2[i].length));
+            data_obj.push(creat_obj_probe(data_probe_group2[i][0], data_probe_group2[i].length));
         }
 
         var margin = {
@@ -76,14 +74,11 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
             bottom: 50,
             left: 150
         },
+
         width = 700 - margin.left - margin.right; 
 
-        if (d3.max(data_obj, function(d) {
-            return d.n_probe_group;
-            }) > 10) {
-                var height = 34.1796875 * d3.max(data_obj, function(d) {
-                    return d.n_probe_group;
-                }) - margin.top - margin.bottom; 
+        if ( data_obj.length  > 5) {
+            var height = 34.1796875 * data_obj.length - margin.top - margin.bottom; 
         } else {
             var height = 34.1796875 * 10 - margin.top - margin.bottom;
         }
@@ -150,7 +145,8 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
             })
             .attr("height", y.rangeBand())
             .attr("fill", function(d) {
-                return graphColor(d.n_probe_group);
+         
+    return graphColor(d.n_probe_group);
             })
             .attr("fill-opacity", .5).style("font-size", "14px")
                 .on("mousedown", function(g, i) {
@@ -159,12 +155,12 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
                     d3.select("#hds_matrix").select("svg").remove();
                     sid = data_obj[i].n_probe_group;
 
-                    histogram_degree_SNPs(file_json, sid, 0, 0);
+                    histogram_degree_SNPs(sid, 0, 0);
 
                     string_html = "{\"directed\": false, \"graph\": [], \"nodes\": [";
 
-                    json_nodes_selected(file_json, sid);
-                    json_links_selected(file_json, sid);
+                    json_nodes_selected(sid);
+                    json_links_selected(sid);
 
                     d3.select("#chart")
                         .selectAll("g circle")
@@ -247,7 +243,7 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
                 });
 
         svg.selectAll(".bar") //show degree as tooltip - title
-        .data(data_obj)
+            .data(data_obj)
             .append("title")
             .text(function(d) {
                 return d.n_edgs;
@@ -275,8 +271,8 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
                 d3.select("#hc").select("svg").remove();
                 sid = data_obj[i].n_probe_group;
                 string_html = "{\"directed\": false, \"graph\": [], \"nodes\": [";
-                json_nodes_selected(file_json, sid);
-                json_links_selected(file_json, sid);
+                json_nodes_selected(sid);
+                json_links_selected(sid);
 
                 d3.select("#chart")
                     .selectAll("g circle")
@@ -348,6 +344,5 @@ function histogram_edges_subgraphId(file_name, if_zoom) {
                         return showInteract(d.ct_id);
                     });
             });
-    });
-}
+};
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ histogram edges X probe_group ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
