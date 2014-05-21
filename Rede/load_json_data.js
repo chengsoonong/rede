fc286c9013c5efcb6ca548b20204541cb26fa347
stefan-sev <stefan.sevelda@hotmail.com>
2014-05-21 15:11:25 +1000

    test ld plot

--- Rede/load_json_data.js
+++ Rede/load_json_data.js
@@ -3,7 +3,10 @@
  * @author stefan.sevelda@hotmail.com (Stefan Sevelda)
  * @author cristovao.casagrande@gmail.com (Cristovao Iglesias)
  * @author chengsoon.ong@unimelb.edu.au (Cheng Ong)
+ *
  */
+
+var ldvar = [];
 
 // function to load the data
 function load_data_json (file_name) {
@@ -23,32 +26,35 @@ function load_data_json (file_name) {
 
     d3.json(file_name, function(json) {
         //function to load the the links
-        json.links.forEach(function(d) {
-            links.push(d);
+        json.ld.forEach(function(d) {
+            ldvar.push(d);
         });
-
-        //for loop to get all available statistical test of the dataset
-        for (var i in json.links[0]) {
-            if (i != "assoc_group" && i != "source" && i != "target" && i !=
-                "probe_group" && i != "ct_id") {
-                    statOptions[i] = i;
-                    st_1.push(i);  
-                }
-        }
-
-        // function to fill the information of the nodes in allNodes
-        json.nodes.forEach(function(d) {
-            allNodes.push(d);
-        });
-        // load the subgraphs of the json file 
-        subgraphs = json.subgraphs;
-        // load the comunities of the json file
-        communities = json.communities;
-        // load the information about the contingency table 
-        cont_table = json.cont_table;
-        
+//        json.links.forEach(function(d) {
+//            links.push(d);
+//        });
+//
+//        //for loop to get all available statistical test of the dataset
+//        for (var i in json.links[0]) {
+//            if (i != "assoc_group" && i != "source" && i != "target" && i !=
+//                "probe_group" && i != "ct_id") {
+//                    statOptions[i] = i;
+//                    st_1.push(i);  
+//                }
+//        }
+//
+//        // function to fill the information of the nodes in allNodes
+//        json.nodes.forEach(function(d) {
+//            allNodes.push(d);
+//        });
+//        // load the subgraphs of the json file 
+//        subgraphs = json.subgraphs;
+//        // load the comunities of the json file
+//        communities = json.communities;
+//        // load the information about the contingency table 
+//        cont_table = json.cont_table;
+//        
         // start the plots
-        upload_json()           
+        //upload_json()           
     });
 };
 
